baa7a0185706733327c7916b37063f65521350b0
Anna Sanova <asanova@Sanovas-iMac.local>
2019-12-04 16:32:10 -0700

    created 2nd CSV, extraneous function

--- list_directory2.py
+++ list_directory2.py
@@ -22,14 +22,16 @@ def main():
     root_dir = Directory(root_name) # initialize root Directory object
 
     check_directory(dir_path, root_dir)
-    #may delete structure_spaces() and add_upwards() fxns
-    # structure_spaces(root_dir) 
+    # structure_spaces(root_dir) # currently unnecessary 
 
     # root_dir.print_tree(0, True) # uncomment for printing to terminal
+
+    count_names = {}
     csv_file = open(root_name + '.csv', 'w')
     csv_writer = csv.writer(csv_file, delimiter=',')
-    create_csv(csv_writer, root_dir, 0)
-
+    create_csv(csv_writer, root_dir, 0, count_names)
+    
+    # show_counts(count_names)
 
 def check_directory(dir_path, root_dir):
     '''Takes in a path and a Directory object to traverse all items within it 
@@ -46,6 +48,7 @@ def check_directory(dir_path, root_dir):
                 root_dir.add_subfile(entry)
 
 def structure_spaces(root_dir):
+    ##may remove
     '''Recursively adds counts of items to each directory with use
     of a helper function. Removes one line at the end to account for one item
     being on the same line as the directory name.
@@ -61,6 +64,7 @@ def structure_spaces(root_dir):
             root_dir.remove_filelines(1)    # after loop, added all subdirectories' counts
 
 def add_upwards(curr_dir, to_add):
+    ##may remove
     '''Helper function for structure_spaces(root_dir).
     Recursively adds the same number of items to each super directory.
     PARAM: current Directory object, int representing number of items'''
@@ -68,7 +72,7 @@ def add_upwards(curr_dir, to_add):
     while (not curr_dir.super_dir()): # if super directory exists
         add_upwards(curr_dir.super_dir(), to_add)
 
-def create_csv(csv_writer, root_dir, count):
+def create_csv(csv_writer, root_dir, count, count_names):
     '''Writes the CSV file. Recursively passes in each subdirectory.
     PARAM: CSV writer tool from csv library, Directory object representing root
         directory, int representing level at which file or folder is at in the tree.
@@ -79,13 +83,42 @@ def create_csv(csv_writer, root_dir, count):
     csv_writer.writerow(curr_line)   # holds only one directory name
 
     for subdir in root_dir.subdirs():
+        counting_helper(count_names, subdir.name() + '/')
         # recursive call allows for items in each folder written immediately after folder name
-        create_csv(csv_writer, subdir, count + 1) 
+        create_csv(csv_writer, subdir, count + 1, count_names) 
 
     for subfile in root_dir.subfiles():
+        counting_helper(count_names, subfile)
         file_line = []
         file_line.extend(['' for i in range(count+1)]) # files always one indent more
         file_line.append(subfile)
         csv_writer.writerow(file_line) # holds only one file name
+
+def counting_helper(count_names, name):
+    '''Helps count number of times a name occurs.
+    PARAM: dictionary to hold counts, string for directory or file name.
+    RETURN: none'''
+    if name in count_names:
+        count_names[name] += 1
+    else: 
+        count_names[name] = 1
+
+def show_counts(count_names):
+    '''Creates a second CSV for showing the count of all repeated items.
+    May make it print the whole file pathway, unsure how that's going to go.
+    PARAM: dictionary holding names and counts.
+    RETURN: none'''
+    csv_file2 = open('counts.csv', 'w')
+    csv_writer = csv.writer(csv_file2, delimiter=',')
+
+    repeat = input('Display all files/folders or only those repeated? ')
+    if ('all' in repeat.lower()):
+        for item in count_names.items():
+            csv_writer.writerow([item[0], item[1]])
+
+    else: # only print repeated ones
+        for item in count_names.items():
+            if (item[1] > 1):
+                csv_writer.writerow([item[0], item[1]])
 
 main()
